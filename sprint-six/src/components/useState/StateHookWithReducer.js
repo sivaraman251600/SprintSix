88685a94d91eb8d5ef50881149446e6bb83432c9
K <251600@NTTDATA.COM>
2022-05-14 18:45:15 +0530

    I am Just Using useState and useReducer combine exercise in my own way

--- sprint-six/src/components/useState/StateHookWithReducer.js
+++ sprint-six/src/components/useState/StateHookWithReducer.js
@@ -1,11 +1,10 @@
 import React, { useReducer, useState } from 'react'
 
 const reducer = (state, action) => {
-    console.log("it's Calling")
     switch(action.type){
         case "SET_NAME" : 
         return {
-            state : action.payload
+            names : action.payload
         }
 
         default : return state
@@ -19,7 +18,7 @@ export const StateHookWithReducer = () => {
     <div>
         <input type={'text'} onChange={(e)=>setName(e.target.value)} />
         <button onClick={()=>dispatch({type:"SET_NAME",payload:name})}>Name</button>
-        <p>{state}</p>
+        <p>{state.names}</p>
     </div>
   )
 }
