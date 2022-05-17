123219d54b9d9350633662e2145785b09764735d
K <251600@NTTDATA.COM>
2022-05-17 15:55:22 +0530

    i finish the beforeEach() method only the rest of the method can't properly illustrated

--- sprint-six/src/test/Counter.test.js
+++ sprint-six/src/test/Counter.test.js
@@ -1,46 +1,50 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react";
+import { cleanup, fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"
 import TestElementOne from '../components/TestElement/TestElementOne'
 
-test('Here I Am Testing the TestElementOne', () => { 
+let getByTestId
+beforeEach(()=>{
     const component = render(<TestElementOne />)
-    const headerEl = component.getByTestId('header')
-    const buttonEl = component.getByTestId('buttonCheck')
+    getByTestId = component.getByTestId
+})
+
+afterAll(()=>{
+    cleanup()
+})
+
+test('Here I Am Testing the TestElementOne', () => { 
+    const headerEl = getByTestId('header')
+    const buttonEl = getByTestId('buttonCheck')
     expect(headerEl.textContent).toBe('My Test State')
     expect(buttonEl.textContent).toBe('Add Name')
  })
 
 test("Test the Display Counter Value with initial 0",()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const displayEl = getByTestId("displayValue")
 
     expect(displayEl.textContent).toBe('0')
 })
 
 test("Test the display subtract button -",()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const subtractBtn = getByTestId("subtract-btn")
 
     expect(subtractBtn.textContent).toBe('-')
 })
 
 test("input type Number value of the ",()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const inputEl = getByTestId('input-value')
 
     expect(inputEl.value).toBe('1')
 })
 
 test("Test the display add button +",()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const subtractBtn = getByTestId("add-btn")
 
     expect(subtractBtn.textContent).toBe('+')
 })
 
 test("Test the input field value as Dynamic", ()=>{
-    const { getByTestId } = render(<TestElementOne />)
     const inputEl = getByTestId("input-value")
 
     expect(inputEl.value).toBe('1')
@@ -55,7 +59,6 @@ test("Test the input field value as Dynamic", ()=>{
 })
 
 test('we going to test the display value as dynamic when i click the add btn',()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const addBtn = getByTestId('add-btn')
     const displayEl = getByTestId("displayValue")
 
@@ -67,7 +70,6 @@ test('we going to test the display value as dynamic when i click the add btn',()
 })
 
 test('when i click the add btn input work fine based on what i type',()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const addBtn = getByTestId('add-btn')
     const displayEl = getByTestId("displayValue")
     const inputEl = getByTestId('input-value')
@@ -84,7 +86,6 @@ test('when i click the add btn input work fine based on what i type',()=>{
 })
 
 test('when i click the subtract btn input work fine based on what i type',()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const subBtn = getByTestId('subtract-btn')
     const displayEl = getByTestId("displayValue")
     const inputEl = getByTestId('input-value')
@@ -101,7 +102,6 @@ test('when i click the subtract btn input work fine based on what i type',()=>{
 })
 
 test('we checking the add and subtract working fine',()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const addBtn = getByTestId('add-btn')
     const subBtn = getByTestId('subtract-btn')
     const displayEl = getByTestId("displayValue")
@@ -121,7 +121,6 @@ test('we checking the add and subtract working fine',()=>{
 })
 
 test('Here i change the color of the display value based on the + and -',()=>{
-    const { getByTestId } = render(<TestElementOne/>)
     const addBtn = getByTestId('add-btn')
     const subBtn = getByTestId('subtract-btn')
     const displayEl = getByTestId("displayValue")
