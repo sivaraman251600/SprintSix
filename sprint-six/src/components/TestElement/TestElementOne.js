af983f4a5148ee90c532f0279ba70bf02122b3cb
K <251600@NTTDATA.COM>
2022-05-17 15:42:54 +0530

    Now Almost test stage is completed

--- sprint-six/src/components/TestElement/TestElementOne.js
+++ sprint-six/src/components/TestElement/TestElementOne.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Testelement.css"
 
 const TestElementOne = () => {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  const subtractHandler = () => {
+    setCounter(counter - inputValue);
+  };
+
+  const addHandler = () => {
+    setCounter(Number(inputValue) + counter);
+  };
+
   return (
     <div>
-        <h1 data-testid="header">My Test State</h1>
-        <button data-testid="buttonCheck">Add Name</button>
+      <h1 data-testid="header">My Test State</h1>
+      <button data-testid="buttonCheck">Add Name</button>
+      <h1 data-testid="displayValue" className={`${counter >= 100 ? 'green' : ''}${counter <= 0 ? 'red' : ''}`}>{counter}</h1>
+      <button data-testid="subtract-btn" onClick={subtractHandler}>
+        -
+      </button>
+      <input
+        data-testid="input-value"
+        id="inputValue"
+        type={"number"}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button data-testid="add-btn" onClick={addHandler}>
+        +
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default TestElementOne
+export default TestElementOne;
--- sprint-six/src/test/Counter.test.js
+++ sprint-six/src/test/Counter.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"
 import TestElementOne from '../components/TestElement/TestElementOne'
 
@@ -10,3 +10,142 @@ test('Here I Am Testing the TestElementOne', () => {
     expect(headerEl.textContent).toBe('My Test State')
     expect(buttonEl.textContent).toBe('Add Name')
  })
+
+test("Test the Display Counter Value with initial 0",()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const displayEl = getByTestId("displayValue")
+
+    expect(displayEl.textContent).toBe('0')
+})
+
+test("Test the display subtract button -",()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const subtractBtn = getByTestId("subtract-btn")
+
+    expect(subtractBtn.textContent).toBe('-')
+})
+
+test("input type Number value of the ",()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const inputEl = getByTestId('input-value')
+
+    expect(inputEl.value).toBe('1')
+})
+
+test("Test the display add button +",()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const subtractBtn = getByTestId("add-btn")
+
+    expect(subtractBtn.textContent).toBe('+')
+})
+
+test("Test the input field value as Dynamic", ()=>{
+    const { getByTestId } = render(<TestElementOne />)
+    const inputEl = getByTestId("input-value")
+
+    expect(inputEl.value).toBe('1')
+
+    fireEvent.change(inputEl,{
+        target:{
+            value : '10'
+        }
+    })
+
+    expect(inputEl.value).toBe('10')
+})
+
+test('we going to test the display value as dynamic when i click the add btn',()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const addBtn = getByTestId('add-btn')
+    const displayEl = getByTestId("displayValue")
+
+    expect(displayEl.textContent).toBe('0')
+    
+    fireEvent.click(addBtn);
+
+    expect(displayEl.textContent).toBe('1')
+})
+
+test('when i click the add btn input work fine based on what i type',()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const addBtn = getByTestId('add-btn')
+    const displayEl = getByTestId("displayValue")
+    const inputEl = getByTestId('input-value')
+
+    fireEvent.change(inputEl,{
+        target:{
+            value:'5'
+        }
+    })
+    
+    fireEvent.click(addBtn);
+
+    expect(displayEl.textContent).toBe('5')
+})
+
+test('when i click the subtract btn input work fine based on what i type',()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const subBtn = getByTestId('subtract-btn')
+    const displayEl = getByTestId("displayValue")
+    const inputEl = getByTestId('input-value')
+
+    fireEvent.change(inputEl,{
+        target:{
+            value:'5'
+        }
+    })
+    
+    fireEvent.click(subBtn);
+
+    expect(displayEl.textContent).toBe('-5')
+})
+
+test('we checking the add and subtract working fine',()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const addBtn = getByTestId('add-btn')
+    const subBtn = getByTestId('subtract-btn')
+    const displayEl = getByTestId("displayValue")
+    const inputEl = getByTestId('input-value')
+
+    fireEvent.change(inputEl,{
+        target:{
+            value:'5'
+        }
+    })
+    
+    fireEvent.click(addBtn);
+    fireEvent.click(addBtn);
+    fireEvent.click(subBtn);
+
+    expect(displayEl.textContent).toBe('5')
+})
+
+test('Here i change the color of the display value based on the + and -',()=>{
+    const { getByTestId } = render(<TestElementOne/>)
+    const addBtn = getByTestId('add-btn')
+    const subBtn = getByTestId('subtract-btn')
+    const displayEl = getByTestId("displayValue")
+    const inputEl = getByTestId('input-value')
+
+    expect(displayEl.textContent).toBe('0')
+
+    fireEvent.change(inputEl,{
+        target:{
+            value:'50'
+        }
+    })
+
+    fireEvent.click(addBtn);
+    fireEvent.click(addBtn);
+
+    expect(displayEl.textContent).toBe('100')
+
+    expect(displayEl.className).toBe('green')
+
+    fireEvent.click(subBtn);
+    fireEvent.click(subBtn);
+    fireEvent.click(subBtn);
+
+    expect(displayEl.className).toBe('red')
+
+})
